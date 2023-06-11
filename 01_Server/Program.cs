e4d85edfaa608e832a3f8ed067f0b9622fcbdf56
lysiuk227 <ninberg.227@gmail.com>
2023-06-11 15:46:11 +0300

    Add project files.

---
+++ 01_Server/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // ...\01_Server\bin\Debug\net7.0\Country-Capital.json
+        string fileName = "Country-Capital.json";
+        string jsonString = File.ReadAllText(fileName);
+        Dictionary<string, string>? CountryCapital = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString);
+
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("172.20.10.2"), 3344);
+        UdpClient server = new UdpClient(endPoint);
+        while(true)
+        {
+            IPEndPoint? clientEndPoint = null;
+            byte[] request = server.Receive(ref clientEndPoint);
+
+            string country = Encoding.UTF8.GetString(request);
+            Console.WriteLine($"Country: {country} from {clientEndPoint}");
+
+            string data="Country not found!";
+            if (CountryCapital?.GetValueOrDefault(country)!=null)
+            {
+                data = country + ": " + CountryCapital.GetValueOrDefault(country);
+            }
+            byte[] response = Encoding.UTF8.GetBytes(data);
+            server.Send(response, response.Length, clientEndPoint);
+        }
+    }
+}
